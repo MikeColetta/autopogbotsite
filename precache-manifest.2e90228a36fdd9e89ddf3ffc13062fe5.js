ca148b77cdcd5ea38aa119862477cfcd3c5bb785
Mike Coletta <coletta.mike@gmail.com>
2021-07-19 15:45:50 -0700

    Updates

---
+++ precache-manifest.2e90228a36fdd9e89ddf3ffc13062fe5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "efe3c7d4cb1b9a07e65c",
+    "url": "/autopogbotsite/static/css/main.0436e089.chunk.css"
+  },
+  {
+    "revision": "efe3c7d4cb1b9a07e65c",
+    "url": "/autopogbotsite/static/js/main.f1d6a5d9.chunk.js"
+  },
+  {
+    "revision": "61c3db641767d3926e0e",
+    "url": "/autopogbotsite/static/js/runtime~main.6a4f5be7.js"
+  },
+  {
+    "revision": "b3fb13f9bf1758bdb4d5",
+    "url": "/autopogbotsite/static/js/2.a249eb77.chunk.js"
+  },
+  {
+    "revision": "94e3b8fb2e3d7ed491066bd1bc486dd0",
+    "url": "/autopogbotsite/static/media/tyler-circle.94e3b8fb.png"
+  },
+  {
+    "revision": "3c1a2d36be87d3178672858941ad340e",
+    "url": "/autopogbotsite/index.html"
+  }
+];
